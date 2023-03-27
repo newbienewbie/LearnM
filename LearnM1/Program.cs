7ada9d99e5de6d878a7f2b4273ab0aa702c49810
YuAnAan <1204546660@qq.com>
2023-03-27 09:11:06 +0800

    修改联系代码不合理的地方

--- LearnM1/Program.cs
+++ LearnM1/Program.cs
@@ -1,22 +1,4 @@
-﻿// System.Console.WriteLine("请输入整数a：");
-// var aStr = Console.ReadLine();
-
-// if(int.TryParse(aStr,out var a){
-//     Console.Write($"无法解析{aStar}");
-//     return ;
-// }
-
-// Console.WriteLine("请输入整数b:");
-// var b = int.Parse(bStr);
-// var bStr = Console.ReadLine();
-// Console.WriteLine($"结果= {a+b}");
-
-
-
-// //提示用户输入a
-// from a in  
-
-using System;
+﻿using System;
 using Itminus.FSharpExtensions;
 using Microsoft.FSharp.Core;
 
@@ -26,10 +8,10 @@ public static class Program{
         //按要求输入a、b的值
         var result = 
             from a in Input($"请输入整数a的值")
-            where 要求输入的值小于10(a, 10)
+            where 要求输入的值大于10(a, 10)
             from b in Input($"请输入整数b的值")
-            where 要求输入的值大于100(b,100)
-            select a  +b;
+            where 要求输入的值小于100(b,100)
+            select a  + b;
         if(result.IsOk){
             Console.WriteLine(result.ResultValue);
         }
@@ -45,21 +27,17 @@ public static class Program{
             }
             return value.ToOkResult<int,string>();
         }
-        FSharpResult<ValueTuple,string> 要求输入的值小于10(int value, int criticalLow)
+        FSharpResult<ValueTuple,string> 要求输入的值大于10(int value, int criticalLow)
         {
-            Console.WriteLine("请输入一个整数：");
             if(value < criticalLow){
-                Console.WriteLine($"要求输入的值小于{criticalLow}");
-                return $"要求输入的值小于10".ToErrResult<ValueTuple, string>();
+                return $"输入a的值小于10".ToErrResult<ValueTuple, string>();
             }
             return new ValueTuple().ToOkResult<ValueTuple, string>();
         }
-        FSharpResult<ValueTuple, string> 要求输入的值大于100(int value, int criticalHigh)
+        FSharpResult<ValueTuple, string> 要求输入的值小于100(int value, int criticalHigh)
         {
-            Console.WriteLine("请输入一个整数：");
             if(value > criticalHigh){
-                Console.WriteLine($"输入的值大于{criticalHigh}");
-                return $"输入的值大于100".ToErrResult<ValueTuple, string>();
+                return $"输入b的值大于100".ToErrResult<ValueTuple, string>();
             }
             return new ValueTuple().ToOkResult<ValueTuple, string>();
         }
